057239bb642dff7b92062f8b44ced3681601d577
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-12-17 23:59:41 +0530

    Express Server Middleware

---
+++ ExpreeServerMiddleware-RequestLogging/index.js
@@ -0,0 +1,18 @@
+// Please don't change the pre-written code.
+
+const express = require("express");
+const app = express();
+
+const logRequest = (req, res, next) => {
+  // Write your code here
+  console.log(`${req.method} ${req.path}`);
+  next();
+};
+
+app.use(logRequest); 
+
+app.get("/", (req, res) => {
+  res.send("Coding Ninjas!");
+});
+
+module.exports = app;
