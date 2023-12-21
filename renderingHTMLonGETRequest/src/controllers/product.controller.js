227be12e54bdb16afacb70d0506ab90a849798d3
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-12-21 14:37:46 +0530

    rendering index.html file on GET request

---
+++ renderingHTMLonGETRequest/src/controllers/product.controller.js
@@ -0,0 +1,8 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+import path from 'path';
+
+export const getProducts = (req, res) => {
+  // Wite your code here
+  return res.sendFile(path.join(path.resolve(), 'src', 'views', 'index.html'));
+};
