470a0a1acde7cb0809c10388fa42bcd48d814fdb
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-12-23 11:03:28 +0530

    implementing expressEjsLayouts

---
+++ blogCreationForm/index.js
@@ -0,0 +1,18 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+import { renderBlogForm } from "./src/controllers/blog.controller.js";
+
+import express from "express";
+import path from "path";
+import expressEjsLayouts from "express-ejs-layouts";
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("src", "views"));
+app.use(expressEjsLayouts);
+
+// Write your code here
+app.get('/createblog', renderBlogForm);
+
+export default app;
