c66ae9a7e17561349fdab00e02d6e0f69a1ffe14
sanjeetsk <ssanjeetkumar67@gmail.com>
2024-02-24 23:59:55 +0530

    socket.io

---
+++ RealTimeChatAppWithSocket/server.js
@@ -0,0 +1,29 @@
+// No need to change the pre-written code
+// Implement the features in io.on() section
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+export const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("Connection made.");
+
+    // Write your code here
+
+    socket.on("disconnect", () => {
+        console.log("Connection disconnected.");
+    });
+});
+
